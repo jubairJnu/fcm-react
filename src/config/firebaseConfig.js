51aa2b99a22ad3a2c9d50a8c250ca646407c1648
Abdullah Al Jubair <jubair2810@gmail.com>
2025-05-21 16:26:06 +0600

    initialize

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZncIo1drOa9U0FwCFB8mg6V40sedTHN0",
+  authDomain: "fcm-practice-4a0c2.firebaseapp.com",
+  projectId: "fcm-practice-4a0c2",
+  storageBucket: "fcm-practice-4a0c2.firebasestorage.app",
+  messagingSenderId: "698732965366",
+  appId: "1:698732965366:web:0fc48b1c2b367bf77debd7",
+};
